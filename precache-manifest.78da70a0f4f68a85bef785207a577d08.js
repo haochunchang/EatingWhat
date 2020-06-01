53ce7c18286166ac522925caf89cad697bdf2fe8
Hao Chun Chang <changhaochun84@gmail.com>
2020-06-01 23:57:49 +0800

    Updates

---
+++ precache-manifest.78da70a0f4f68a85bef785207a577d08.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71b6c9e113df3f495cc7aa13be4af9d",
+    "url": "/EatingWhat/index.html"
+  },
+  {
+    "revision": "18d459d68e36a9de46f5",
+    "url": "/EatingWhat/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6b0097ff9dc01c03cb47",
+    "url": "/EatingWhat/static/css/main.cc28c14e.chunk.css"
+  },
+  {
+    "revision": "18d459d68e36a9de46f5",
+    "url": "/EatingWhat/static/js/2.7628aa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EatingWhat/static/js/2.7628aa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0097ff9dc01c03cb47",
+    "url": "/EatingWhat/static/js/main.073a91b7.chunk.js"
+  },
+  {
+    "revision": "71498e2f42597eb42626",
+    "url": "/EatingWhat/static/js/runtime-main.108e3c9e.js"
+  },
+  {
+    "revision": "2d16b55cfd7d3e3aa1d528c48f81aa7b",
+    "url": "/EatingWhat/static/media/FontsFreeNetObelixProcyr.2d16b55c.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EatingWhat/static/media/logo.5d5d9eef.svg"
+  }
+]);
